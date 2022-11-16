e5ca6b467f5cdef3cd9f6fcddce0ad1022384f46
ahmet479 <115145728+ahmet479@users.noreply.github.com>
2022-11-16 22:36:42 +0100

    Add files via upload

---
+++ JavaDers20/MainClass.java
@@ -0,0 +1,15 @@
+package JavaDers20;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		String bilgi = "Cocuk \"Benim adim Kerem \" dedi";
+		System.out.println(bilgi);
+		
+		String bilgiler = "Ahmet\nEmine\nZulal";
+		System.out.println(bilgiler);
+
+		
+	}
+
+}
