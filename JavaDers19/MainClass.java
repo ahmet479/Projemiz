9e21a1192f4ce6418b5abfb395463da94517565d
ahmet479 <115145728+ahmet479@users.noreply.github.com>
2022-11-16 22:36:25 +0100

    Add files via upload

---
+++ JavaDers19/MainClass.java
@@ -0,0 +1,22 @@
+package JavaDers19;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		String diller = "arapca, farsca, turkce, ingilizce";
+		/*String dil1  = diller.substring(0,6);
+		String dil2 = diller.substring(8,15);
+		String dil3 = diller.substring(16,23);
+		String dil4 = diller.substring(24,33);
+		
+		System.out.println(dil1);
+		System.out.println(dil2);
+		System.out.println(dil3);
+		System.out.println(dil4);*/
+		
+		String dilim = diller.substring(20);
+		System.out.println(dilim);
+		
+	}
+
+}
